81f7d9e10eeebfa59e766c073517d1a7a9a27351
fabiooo4 <fabioirimie@gmail.com>
2022-06-23 16:23:41 +0200

    Login screen

--- webapp/controller/Rapportini.controller.js
+++
@@ -1,6 +0,0 @@
-sap.ui.define(["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"], function (Controller) {
-    "use strict";
-    return Controller.extend("sap.ui.demo.walkthrough.controller.Rapportini", {
-
-    });
-});
